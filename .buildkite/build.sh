a3b4c75c3f77fbce7da20f0d0c0a28aafb8db5a1
gustavderdrache <aford@forumone.com>
2019-10-29 17:38:51 -0400

    Fix permissions on versions binary

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -9,9 +9,19 @@ repository_name=f1-ext-install
 
 declare -a versions
 
+chmod +x target/x86_64-unknown-linux-musl/debug/versions
 coproc VERSIONS { target/x86_64-unknown-linux-musl/debug/versions; }
 
-mapfile -t -u "${VERSIONS[0]}" versions
+# Immediately save the PID of the VERSIONS coproc
+id="$VERSIONS_PID"
+
+# Clone the VERSIONS coproc's stdout
+exec 4<&"${VERSIONS[0]}"
+
+# Propagate non-zero exits of the 'versions' command
+wait "$id"
+
+mapfile -t -u 4 versions
 
 echo '--- :docker: Build'
 docker build "$project_directory" --tag "$repository_name:latest"
