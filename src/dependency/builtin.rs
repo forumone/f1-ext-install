99217b7ed8d971255fa0a476032917c895a48df9
gustavderdrache <aford@forumone.com>
2019-11-25 10:49:30 -0500

    Allow externally-configured registry data

--- src/dependency/builtin.rs
+++ src/dependency/builtin.rs
@@ -3,41 +3,60 @@
 use lazy_static::lazy_static;
 use maplit::btreemap;
 use nom::{combinator::map, error::ParseError, IResult};
+use serde::Deserialize;
 use std::{collections::BTreeMap, str::FromStr};
 
-use super::{parse, StringList};
+use super::parse;
+
+/// Represents the data for a PHP builtin extension.
+#[derive(Clone, Debug, Default, Deserialize)]
+pub struct BuiltinData {
+    /// The list of external packages (if any) this extension needs.
+    #[serde(default)]
+    packages: Option<Vec<String>>,
+    /// Represents the arguments to pass to `docker-php-ext-configure`, if that utility
+    /// needs to be called.
+    #[serde(default)]
+    configure_cmd: Option<Vec<String>>,
+}
 
 /// Represents the information needed for a PHP builtin extension.
 #[derive(Clone, Debug)]
 pub struct Builtin {
     /// The name of this extension, as used by the `docker-php-ext-install` utility.
     name: String,
-    /// The list of external packages (if any) this extension needs.
-    packages: Option<StringList<'static>>,
-    /// Represents the arguments to pass to `docker-php-ext-configure`, if that utility
-    /// needs to be called.
-    configure_cmd: Option<StringList<'static>>,
+
+    /// The data for this builtin.
+    data: BuiltinData,
 }
 
 impl Builtin {
     /// Creates a new builtin by assuming that no external packages or configuration is needed.
-    pub fn new(name: String) -> Self {
+    pub fn new<S>(name: S) -> Self
+    where
+        S: Into<String>,
+    {
         Self {
-            name,
-            packages: None,
-            configure_cmd: None,
+            name: name.into(),
+            data: BuiltinData::default(),
         }
     }
 
+    /// Replaces this extension's data with the given data.
+    fn with_data(mut self, data: BuiltinData) -> Self {
+        self.data = data;
+        self
+    }
+
     /// Adds external package dependencies to this builtin.
-    pub fn with_packages(mut self, packages: StringList<'static>) -> Self {
-        self.packages = Some(packages);
+    pub fn with_packages(mut self, packages: Vec<String>) -> Self {
+        self.data.packages = Some(packages);
         self
     }
 
     /// Adds `docker-php-ext-configure` args to this builtin.
-    pub fn with_configure_cmd(mut self, configure_cmd: StringList<'static>) -> Self {
-        self.configure_cmd = Some(configure_cmd);
+    pub fn with_configure_cmd(mut self, configure_cmd: Vec<String>) -> Self {
+        self.data.configure_cmd = Some(configure_cmd);
         self
     }
 
@@ -47,13 +66,13 @@ impl Builtin {
     }
 
     /// Returns the list of external packages (if any) needed by this builtin.
-    pub fn packages(&self) -> &Option<StringList<'_>> {
-        &self.packages
+    pub fn packages(&self) -> Option<&[String]> {
+        self.data.packages.as_ref().map(AsRef::as_ref)
     }
 
     /// Returns the configure command (if any) needed by this builtin.
-    pub fn configure_cmd(&self) -> &Option<StringList<'_>> {
-        &self.configure_cmd
+    pub fn configure_cmd(&self) -> Option<&[String]> {
+        self.data.configure_cmd.as_ref().map(AsRef::as_ref)
     }
 
     /// Attempts to parse the name of a builtin from the input.
@@ -65,28 +84,42 @@ impl Builtin {
     ///
     /// [`Dependency`]: crate::dependency::Dependency
     pub fn parse<'a, E: ParseError<&'a str>>(input: &'a str) -> IResult<&'a str, Self, E> {
-        let parser = map(parse::name, |name: &str| {
-            REGISTRY
-                .get(name)
-                .cloned()
-                .unwrap_or_else(|| Builtin::new(name.to_owned()))
-        });
+        let parser = map(parse::name, find_builtin);
 
         parser(input)
     }
 }
 
+/// Finds (or creates) a builtin by name.
+fn find_builtin(name: &str) -> Builtin {
+    if let Some(found) = REGISTRY.get(name) {
+        return found.clone();
+    }
+
+    let prefix = format!("F1_BUILTIN_{}_", name.to_ascii_uppercase());
+
+    if let Ok(data) = envy::prefixed(prefix).from_env() {
+        return Builtin::new(name).with_data(data);
+    }
+
+    Builtin::new(name)
+}
+
 lazy_static! {
     static ref REGISTRY: BTreeMap<&'static str, Builtin> = btreemap! {
-        "gd" => Builtin::new("gd".to_owned())
-            .with_packages(&["coreutils", "freetype-dev", "libjpeg-turbo-dev"])
-            .with_configure_cmd(&[
-                "--with-freetype-dir=/usr/include/",
-                "--with-jpeg-dir=/usr/include/",
-                "--with-png-dir=/usr/include/",
+        "gd" => Builtin::new("gd")
+            .with_packages(vec![
+                "coreutils".into(),
+                "freetype-dev".into(),
+                "libjpeg-turbo-dev".into()
+            ])
+            .with_configure_cmd(vec![
+                "--with-freetype-dir=/usr/include/".into(),
+                "--with-jpeg-dir=/usr/include/".into(),
+                "--with-png-dir=/usr/include/".into(),
             ]),
-        "soap" => Builtin::new("soap".to_owned()).with_packages(&["libxml2-dev"]),
-        "zip" => Builtin::new("zip".to_owned()).with_packages(&["libzip-dev"]),
+        "soap" => Builtin::new("soap").with_packages(vec!["libxml2-dev".into()]),
+        "zip" => Builtin::new("zip").with_packages(vec!["libzip-dev".into()]),
     };
 }
 
---
+++ tests/builtin.rs
@@ -0,0 +1,91 @@
+use indoc::indoc;
+
+mod common;
+use common::{build_image, connect, PHP_VERSIONS, tag_for_test};
+
+const REGISTRY_DOCKERFILE: &str = indoc!(
+    r#"ARG PHP_VERSION
+       FROM php:${PHP_VERSION}-cli-alpine
+
+       COPY f1-ext-install /usr/bin/
+       RUN chmod +x /usr/bin/f1-ext-install
+
+       ARG PACKAGE_NAME
+       ENV PACKAGE_NAME=${PACKAGE_NAME}
+
+       RUN f1-ext-install "builtin:${PACKAGE_NAME}" >/dev/null
+       RUN php -r "if (!extension_loaded(getenv('PACKAGE_NAME'))) exit(1);""#
+);
+
+#[test]
+fn test_builtins_from_registry() {
+    let client = connect();
+
+    let builtins = &["gd", "soap", "zip"];
+
+    for &builtin in builtins {
+        for &version in PHP_VERSIONS {
+            let tag = tag_for_test("builtin", builtin, version);
+
+            build_image(
+                &client,
+                REGISTRY_DOCKERFILE,
+                &[("PACKAGE_NAME", builtin), ("PHP_VERSION", version)],
+                &tag,
+            );
+        }
+    }
+}
+
+const EXTERNAL_DOCKERFILE: &str = indoc!(
+    r#"ARG PHP_VERSION
+       FROM php:${PHP_VERSION}-cli-alpine
+
+       COPY f1-ext-install /usr/bin/
+       RUN chmod +x /usr/bin/f1-ext-install
+
+       ARG PACKAGE_NAME
+       ENV PACKAGE_NAME=${PACKAGE_NAME}
+
+       ARG PACKAGES_KEY
+       ARG PACKAGES_VALUE
+       ENV ${PACKAGES_KEY:-_p}=${PACKAGES_VALUE}
+
+       ARG CONFIGURE_KEY
+       ARG CONFIGURE_VALUE
+       ENV ${CONFIGURE_KEY:-_c}=${CONFIGURE_VALUE}
+
+       RUN f1-ext-install "builtin:${PACKAGE_NAME}" >/dev/null
+       RUN php -r "if (!extension_loaded(getenv('PACKAGE_NAME'))) exit(1);""#
+);
+
+#[test]
+fn test_external_builtin_config() {
+    let client = connect();
+
+    let packages = &[(
+        "ldap",
+        ("F1_BUILTIN_LDAP_PACKAGES", "openldap-dev"),
+        ("F1_BUILTIN_LDAP_CONFIGURE_ARGS", "--with-ldap"),
+    )];
+
+    for &(package, (packages_key, packages_value), (configure_key, configure_value)) in packages {
+        for &version in PHP_VERSIONS {
+            let tag = tag_for_test("builtin", package, version);
+
+            build_image(
+                &client,
+                EXTERNAL_DOCKERFILE,
+                &[
+                    ("PACKAGE_NAME", package),
+                    ("PHP_VERSION", version),
+                    ("PACKAGES_KEY", packages_key),
+                    ("PACKAGES_VALUE", packages_value),
+                    ("CONFIGURE_KEY", configure_key),
+                    ("CONFIGURE_VALUE", configure_value),
+                ],
+                &tag,
+            );
+        }
+    }
+}
